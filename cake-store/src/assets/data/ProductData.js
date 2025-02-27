2d4e82895d95683def85ff2cd631d0f6551441a4
quankudo <quannguyen2004aa@gmail.com>
2025-02-28 00:29:04 +0700

    commit update layout home page

--- cake-store/src/assets/data/ProductData.js
+++ cake-store/src/assets/data/ProductData.js
@@ -32,4 +32,40 @@ export const products = [
       description: "Hương vị Ý đặc trưng, bánh Tiramisu thơm ngon, quyến rũ.",
     }
   ];
+
+export const IntroductionProduct = [
+  {
+    imageUrl: "https://mauweb.monamedia.net/monacake/wp-content/uploads/2018/06/banner-1-550x420.png",
+    title: "Chân thành mang tết yêu thương",
+    description: "Hơn ai hết xuất thân từ vùng quê, từ nhỏ tôi đã yêu đã hiểu đã quý trọng cái không khí tết trung thu, đã biết nó không phải chỉ là một dịp lễ hội một năm có 1 lần mà là một 1 lần đó người ta nhận được gì và cho đi những gì…"
+  },
+  {
+    imageUrl: "https://mauweb.monamedia.net/monacake/wp-content/uploads/2018/06/banner-2.png",
+    title: "Thiện tâm thiện ý sức khỏe người dùng",
+    description: "Làm trong ngành nhà hàng khách sạn nên từ lâu tôi đã rất kĩ tính với chất lượng món ăn, khi tôi học nấu ăn tôi cũng đòi hỏi rất khắt khe ở mình, có thể tạm chấp nhận nấu chưa ngon nhưng tuyệt nhiên không cho mình cẩu thả về chất lượng…"
+  },
+  {
+    imageUrl: "https://mauweb.monamedia.net/monacake/wp-content/uploads/2018/06/banner-3.png",
+    title: "Mỹ miều hộp bánh đài trang trong ngoài",
+    description: "Tôi chủ bánh MonaCake với kinh nghiệm 5 năm trong ngành nhà hàng khách sạn tự tin rằng mình luôn đưa ra những sản phẩm hoàn hảo từ nội dung đến hình thức…"
+  },
+]
+
+export const Categories = [
+  {
+    title: "Theo hương vị 🍫🍓"
+  },
+  {
+    title: "Theo phong cách 🎨"
+  },
+  {
+    title: "Theo độ tuổi 🎂"
+  },
+  {
+    title: "Theo chủ đề 🎭"
+  },
+  {
+    title: "Theo dịp đặc biệt 🎉"
+  }
+]
   
--- cake-store/src/pages/customer_view/HomePage.jsx
+++ cake-store/src/pages/customer_view/HomePage.jsx
@@ -4,7 +4,9 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import Cake from '../../components/customer_view/Cake';
-import {products} from '../../assets/data/ProductData'
+import { IoHeartCircleOutline } from "react-icons/io5";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import {IntroductionProduct, Categories, products} from '../../assets/data/ProductData'
 
 const Home = () => {
 
@@ -39,18 +41,30 @@ const Home = () => {
 
   return (
     <div className='mt-10'>
-      {/* Banner */}
-      <Slider {...settings}>
-        <div>
-          <img decoding="async" src={BannerImage} className='w-full h-[30rem]' alt="Slider Image 1" title="Thư viện carousel được sử dụng để tạo các slideshow (React ) 2 học lập trình frontend, fullstack hà nội"/>
-        </div>
-        <div>
-          <img decoding="async" src={BannerImage} className='w-full h-[30rem]' alt="Slider Image 2" title="Thư viện carousel được sử dụng để tạo các slideshow (React ) 3 học lập trình frontend, fullstack hà nội"/>
-        </div>
-        <div>
-          <img decoding="async" src={BannerImage} className='w-full h-[30rem]' alt="Slider Image 3" title="Thư viện carousel được sử dụng để tạo các slideshow (React ) 4 học lập trình frontend, fullstack hà nội"/>
-        </div>
-      </Slider>
+      <div className='flex px-[7%]'>
+        <nav className='w-[20%] bg-pink-100'>
+          {
+            Categories.map((item, index) => (
+              <div className='flex items-center justify-between px-4 py-2 cursor-pointer border-b border-b-gray-400'>
+                <p className='text-black text-[16px]'>{item.title}</p>
+                <span><MdKeyboardArrowRight /></span>
+              </div>
+            ))
+          }
+        </nav>
+        {/* Banner */}
+        <Slider {...settings} className='w-[80%]'>
+          <div>
+            <img decoding="async" src={BannerImage} className='w-full h-[30rem]' alt="Slider Image 1" title="Thư viện carousel được sử dụng để tạo các slideshow (React ) 2 học lập trình frontend, fullstack hà nội"/>
+          </div>
+          <div>
+            <img decoding="async" src={BannerImage} className='w-full h-[30rem]' alt="Slider Image 2" title="Thư viện carousel được sử dụng để tạo các slideshow (React ) 3 học lập trình frontend, fullstack hà nội"/>
+          </div>
+          <div>
+            <img decoding="async" src={BannerImage} className='w-full h-[30rem]' alt="Slider Image 3" title="Thư viện carousel được sử dụng để tạo các slideshow (React ) 4 học lập trình frontend, fullstack hà nội"/>
+          </div>
+        </Slider>
+      </div>
 
       <div className='mt-10 px-[7%]'>
         <h2 className='text-2xl text-center italic my-6'>Sản phẩm nổi bật</h2>
@@ -61,6 +75,35 @@ const Home = () => {
               price={product.price} discountPrice={product.discountPrice} imageUrl={product.imageUrl} />
           ))
         }
+        </div>
+      </div>
+
+      <div className='mt-10 px-[7%]'>
+        <div className='text-center'>
+          <h3 className='text-3xl text-pink-600'>Cái tên Mona Cake chúng tôi đặt hết tấm lòng mình vào đấy</h3>
+          <p className='text-black text-xl my-5 italic'>Thiên là trời, lương trong lương thiện, thiên lương nhằm hướng đến giá trị cao nhất trong cuộc sống</p>
+          <h4 className='text-pink-600 text-3xl'>CHÂN - THIỆN - MỸ</h4>
+          <div className='relative w-40 mx-auto mt-3'>
+            <div className='absolute top-[50%] left-0 l translate-y-[-50%] w-16 h-[3px] bg-pink-600'></div>
+            <IoHeartCircleOutline className='mx-auto text-pink-600' size={24}/>
+            <div className='absolute top-[50%] right-0 translate-y-[-50%] w-16 h-[3px] bg-pink-600'></div>
+          </div>
+        </div>
+        <div className='mt-10 px-20'>
+          {
+            IntroductionProduct.map((item, index)=>(
+              <div className={`flex items-center ${index%2==1?"flex-row-reverse": ""}`}>
+                <div className='w-[50%]'>
+                  <img src={item.imageUrl} alt="" className='object-cover h-[300px] w-full'/>
+                </div>
+                <div className='flex flex-col w-[50%] gap-3 items-start px-10'>
+                  <h3 className='text-2xl text-pink-600'>{item.title}</h3>
+                  <p className='text-[16px]'>{item.description}</p>
+                  <a className='text-white text-[16px] bg-pink-500 px-4 py-2 rounded'>Đọc thêm</a>
+                </div>
+              </div>
+            ))
+          }
         </div>
       </div>
     </div>
